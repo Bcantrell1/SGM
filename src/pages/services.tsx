083fa44b1c3b689585f1a4aa909f6b9813a5bb1b
bribri546@gmail.com <bribri546@gmail.com>
2022-09-29 19:34:02 -0700

    more vercel revisions v1

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -2,7 +2,6 @@ import {
 	Box,
 	Center,
 	Container,
-	createIcon,
 	Flex,
 	Heading,
 	Icon,
--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,12 +1,10 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
-import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
 
 /**
  * Replace this with an object if you want to pass things to createContextInner
  */
-type CreateContextOptions = Record<string, never>;
 
 /** Use this helper for:
  * - testing, where we dont have to Mock Next.js' req/res
