25e2b592d3265bd3a51c4dfd8294f6d5c767584e
bribri546@gmail.com <bribri546@gmail.com>
2022-11-03 14:56:22 -0700

    typo removal

--- src/components/layout/navBar.tsx
+++ src/components/layout/navBar.tsx
@@ -26,7 +26,6 @@ export default function NavBar() {
 	colorMode = 'dark';
   return (
     <>
-		{colorMode}
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <IconButton
